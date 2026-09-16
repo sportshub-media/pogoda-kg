f06d1618d006f8ca761d7855fa6be505da656af9
Tatul Baghdasaryan <tatulbagdasaryan9@Tatuls-MacBook-Pro-2.local>
2026-09-16 15:24:08 +0400

    Limit browser caching for site JavaScript

--- build.js
+++ build.js
@@ -647,6 +647,9 @@ const headers = `/*
   Strict-Transport-Security: max-age=31536000; includeSubDomains
   Permissions-Policy: geolocation=(self), microphone=(), camera=(), payment=()
   Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com https://mc.yandex.ru https://unpkg.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://unpkg.com; font-src 'self' data: https://fonts.gstatic.com; img-src 'self' data: blob: https:; connect-src 'self' https://api.open-meteo.com https://www.googletagmanager.com https://www.google-analytics.com https://mc.yandex.ru https://yandex.ru https://cm.g.doubleclick.net https://stats.g.doubleclick.net wss://mc.yandex.ru; frame-src https://www.googletagmanager.com https://mc.yandex.ru; object-src 'none'; base-uri 'self'; form-action 'self'
+
+/js/*
+  Cache-Control: public, max-age=300, must-revalidate
 `;
 fs.writeFileSync(path.join(OUT_DIR, '_headers'), headers);
 
